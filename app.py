335adbabc0a2595b0708e8f890d0e936d32c9e7d
Elias Montilla <eliasbest634@gmail.com>
2023-11-17 19:19:05 +0000

    finish game

--- app.py
+++ app.py
@@ -3,9 +3,33 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
+import random
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+mapa = {
+    'piedra': '֎',
+    'tijera': '✂',
+    'papel': '☐'
+}
+
+@app.route('/jugar/<eleccion>', methods=['GET'])
+def jugar(eleccion):
+    opciones = ['piedra', 'papel', 'tijera']
+    resultado = random.choice(opciones)
+    ganador = determinar_ganador(eleccion, resultado)
+    return jsonify({'eleccion': mapa[eleccion], 'resultado': mapa[resultado], 'ganador': ganador})
+
+def determinar_ganador(eleccion, resultado):
+    if eleccion == resultado:
+        return '🜲 Intenta de nuevo 🜲'
+    if (eleccion == 'piedra' and resultado == 'tijera') or \
+       (eleccion == 'tijera' and resultado == 'papel') or \
+       (eleccion == 'papel' and resultado == 'piedra'):
+        return 'Ganaste ✔'
+    else:
+        return 'Perdiste ✘'
